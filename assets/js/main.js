ae4a69fe374fe47f6c739d8fbd91adb6f63f2988
Antony Yang <mrant0ny@gmail.com>
2015-08-10 20:52:29 -0700

    modularize js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,81 +1,34 @@
-$(document).ready(function(){
-			
-	$(window).resize(function(){
-		var $valoreScroll = $(window).scrollTop();
-		var $valoreDiv = $('#intro').height();
-	}).resize();
-	
-	$(window).scroll(function(){
-		var $valoreScroll = $(window).scrollTop();
-		var $valoreDiv = $('#intro').height();
-		var $controllo = $valoreDiv - $valoreScroll;
-		if($controllo <= 3){
-			$('#header').stop().animate({top: "0px"}, 500);
-			$('#scrollTop').stop().animate({bottom: "20px"}, 500);
-		} else if($controllo > 0) {
-			$('#header').stop().animate({top: "-100px"}, 500);
-			$('#scrollTop').stop().animate({bottom: "-60px"}, 500);
-		}
-	});
-
-	// Navigate to sections
-	$('img.scrollTop').click(
-		function(){
-			$.scrollTo('#intro', 1500, {easing:'easeInOutExpo'});
-	});
-	$('img.scrollAbout, li.aboutLink').click(
-		function(){
-			$.scrollTo('#about', 1500, {easing:'easeInOutExpo'});
-	});
-	$('li.experienceLink').click(
-		function(){
-			$.scrollTo('#experience', 1500, {easing:'easeInOutExpo'});
-	});
-	$('li.projectLink').click(
-		function(){
-			$.scrollTo('#projects', 1500, {easing:'easeInOutExpo'});
-	});
-	$('li.contactLink').click(
-		function(){
-			$.scrollTo('#contact', 1500, {easing:'easeInOutExpo'});
-	});
+jQuery(document).ready(function($) {
 
 
-	// Projects
-	$('.to002').click(
-		function(){
-			$.scrollTo('#002', 1500, {easing:'easeInOutExpo'});
-			return false;
-	});
-	$('.to003').click(
-		function(){
-			$.scrollTo('#003', 1500, {easing:'easeInOutExpo'});
-			return false;
-	});
-	$('.to004').click(
-		function(){
-			$.scrollTo('#004', 1500, {easing:'easeInOutExpo'});
-			return false;
-	});
-	$('.to005').click(
-		function(){
-			$.scrollTo('#005', 1500, {easing:'easeInOutExpo'});
-			return false;
-	});
-	$('.to006').click(
-		function(){
-			$.scrollTo('#006', 1500, {easing:'easeInOutExpo'});
-			return false;
-	});
+    /*======= Skillset *=======*/
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
 
-	/*/ --- TIMELINE --- /*/
-	$('ul.flaTimeline').flaTimeline({
-		color: '#3dd0ac'
-	});
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
 
-	//testClick
-	$("span.testclick").hover(function () {
-			$(this).toggle("fadeIn");
-		});
-			
+    });
+
+    /* Bootstrap tooltip for Number */
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+    
+    /* Github Activity Feed */
+    GitHubActivity.feed({ username: "antyang", selector: "#ghfeed" });
+
+
 });
